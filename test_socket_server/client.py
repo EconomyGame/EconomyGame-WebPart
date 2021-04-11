85a71cd5c18b7a14c61d6863668dc57009b2fe04
sevadp <45359075+sevadp@users.noreply.github.com>
2021-04-11 12:16:39 +0300

    updated req

--- test_socket_server/client.py
+++ test_socket_server/client.py
@@ -3,24 +3,21 @@ import socketio
 
 sio = socketio.AsyncClient()
 
-
 @sio.event
 async def connect():
     print('connection established')
 
-
 @sio.event
-async def update_game(data):
+async def my_message(data):
     print('message received with ', data)
-
+    await sio.emit('my response', {'response': 'my response'})
 
 @sio.event
 async def disconnect():
     print('disconnected from server')
 
-
 async def main():
-    await sio.connect('http://tp-project2021.herokuapp.com')
+    await sio.connect('http://tp-project2021.herokuapp.com/')
     await sio.wait()
 
 if __name__ == '__main__':
