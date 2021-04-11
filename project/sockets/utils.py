9b636463eb3d091e3f1a0aa3a7c47816761d7165
sevadp <45359075+sevadp@users.noreply.github.com>
2021-04-11 15:37:10 +0300

    Revert "Update utils.py"
    This reverts commit 07469ad56df4e72f568a69c4f6ceda8eaccec559.

--- project/sockets/utils.py
+++ project/sockets/utils.py
@@ -2,4 +2,5 @@ from project.app import socketio, app
 
 
 def broadcast_game(prepared_game_object):
+    print(prepared_game_object)
     socketio.emit('update_game', prepared_game_object, broadcast=True)
