07469ad56df4e72f568a69c4f6ceda8eaccec559
sevadp <45359075+sevadp@users.noreply.github.com>
2021-04-11 15:36:16 +0300

    Update utils.py

--- project/sockets/utils.py
+++ project/sockets/utils.py
@@ -2,5 +2,4 @@ from project.app import socketio, app
 
 
 def broadcast_game(prepared_game_object):
-    print(prepared_game_object)
     socketio.emit('update_game', prepared_game_object, broadcast=True)
